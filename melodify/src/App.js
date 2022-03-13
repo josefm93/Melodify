b1e2291d8e4593a36937adf0e6c7660e9fbb4bb7
Ethan Lee <33e.lee@gmail.com>
2022-03-12 17:58:42 -0800

    add navigation router

--- melodify/src/App.js
+++ melodify/src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Lobby</Link>
+            </li>
+            <li>
+              <Link to="/room">Room</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Lobby/>}/>
+          <Route path="/room" element={<Room/>}/>
+        </Routes>
+      </div>
   );
 }
 
+function Room() {
+  return <h2>Rooom</h2>;
+}
+
+function Lobby() {
+  return <h2>Lobby</h2>;
+}
 export default App;
