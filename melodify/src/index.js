6d634ad4c30554e3d648f3ced38a89b68fb2767f
Ethan Lee <33e.lee@gmail.com>
2022-03-12 18:22:44 -0800

    add gotham fonts: title, body, light

--- melodify/src/index.js
+++ melodify/src/index.js
@@ -5,6 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom"
 
+import './fonts/Gotham/Gotham-Black.otf';
+import './fonts/Gotham/Gotham-Light.otf';
+import './fonts/Gotham/Gotham-Medium.otf';
+
+
 ReactDOM.render(
   <React.StrictMode>
 
