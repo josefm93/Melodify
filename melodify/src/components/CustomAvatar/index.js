2b42344627edfb5e022f0837dfaef4a2d1ad012b
Ethan Lee <33e.lee@gmail.com>
2022-03-13 06:10:11 -0700

    add avatar, header, link to room page

--- melodify/src/components/CustomAvatar/index.js
+++ melodify/src/components/CustomAvatar/index.js
@@ -1,12 +1,12 @@
 import Icon from "@ant-design/icons";
 
-import { placeholders } from "./constant";
+import { avatarsList } from "./constant";
 import { StyledAvatar } from "./styled";
 
 const CustomAvatar = ({ photo, background = "white", size = "55px" }) => {
-    const getRandomAvatar = (placeholders) => {
-        const index = Math.floor(Math.random() * placeholders.length);
-        return placeholders[index];
+    const getRandomAvatar = (avatarsList) => {
+        const index = Math.floor(Math.random() * avatarsList.length);
+        return avatarsList[index];
     };
 
     return (
@@ -14,7 +14,7 @@ const CustomAvatar = ({ photo, background = "white", size = "55px" }) => {
             className="custom-avatar"
             style={{ background, height: size, width: size }}
             src={photo && photo}
-            icon={!photo && <Icon component={getRandomAvatar(placeholders)} />}
+            icon={!photo && <Icon component={getRandomAvatar(avatarsList)} />}
         />
     );
 };
